8f5ba05464a4184360fa0211225a08ce3766ee2c
alexaoh <alexaoh@users.noreply.github.com>
2023-04-04 11:25:10 +0200

    Looked at results from Markov with hyperparameters acc. to Kotelnikov.
    Results look worse! Go back to previous hyperparameters!

--- bash_scripts/generate_tvae_data.sh
+++ bash_scripts/generate_tvae_data.sh
@@ -2,8 +2,9 @@
 DIR="$(cd "$(dirname "$0")" && pwd)" # Finds the directory we are working in.
 
 # Generate data from TVAE. 
-nice python $DIR/../TVAE/generate_data_AD.py -s 1234 -t True --compress-dims 256 512 --decompress-dims 256 512 -b 4096 -e 300 --loss-factor 1 --embedding-dim 512
+nice python $DIR/../TVAE/generate_data_AD.py -s 1234 -t True --compress-dims 256 512 --decompress-dims 256 512 -b 256 -e 300 --loss-factor 1 --embedding-dim 512
 # -e 30000
+# -b 4096
 nice python $DIR/../TVAE/generate_data_CH.py -s 1234 -t True --compress-dims 256 512 --decompress-dims 256 512 -b 256 -e 300 --loss-factor 2 --embedding-dim 512
 # -e 30000
 nice python $DIR/../TVAE/generate_data_DI.py -s 1234 -t True --compress-dims 54 512 512 512 --decompress-dims 54 512 512 512 -b 256 -e 300 --loss-factor 6 --embedding-dim 512
