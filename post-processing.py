076a8d58355f00a60cb7b3305c2cd731cce504e5
alexaoh <alexaoh@users.noreply.github.com>
2023-06-16 23:06:41 +0200

    Deleted many old development/experimental and early stage files.

--- post-processing.py
+++
@@ -1,9 +0,0 @@
-# Step 3 of MCCE: post-processing of possible counterfactuals.
-
-def gower(x, y):
-    """Calculate Gower's distance."""
-    pass
-
-def sparsity(x, y):
-    """Calculate sparsity..."
-    pass
